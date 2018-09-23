577dd6faf3d455d2dd02159357d43e43ccf98182
Andreas Storesund Madsen <andreas@asmadsen.no>
2018-09-23 15:44:07 +0200

    HttpStatus enum and HttpServer

---
+++ src/main/no/kristiania/pgr200/assignment/one/Exceptions/InvalidHttpStatusCodeException.java
@@ -0,0 +1,4 @@
+package no.kristiania.pgr200.assignment.one.Exceptions;
+
+public class InvalidHttpStatusCodeException extends RuntimeException {
+}
--- src/main/no/kristiania/pgr200/assignment/one/HttpServer.java
+++ src/main/no/kristiania/pgr200/assignment/one/HttpServer.java
@@ -1,7 +1,5 @@
 package no.kristiania.pgr200.assignment.one;
 
-import com.sun.security.ntlm.Server;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -10,31 +8,38 @@ import java.net.Socket;
 
 public class HttpServer {
 
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer();
+        server.runServer(8080);
+    }
+
     private void listen(ServerSocket serverSocket) {
         while (true) {
-            try (Socket socket = serverSocket.accept()) {
-                InputStream inputStream = socket.getInputStream();
-                OutputStream outputStream = socket.getOutputStream();
+            new Thread(() -> {
+                try (Socket socket = serverSocket.accept()) {
+                    InputStream inputStream = socket.getInputStream(); // Request
+                    OutputStream outputStream = socket.getOutputStream(); // Response
 
-                HttpRequest request = new HttpRequest(inputStream);
+                    HttpRequest request = new HttpRequest(inputStream);
 
 
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         }
     }
 
     public void runServer() {
-        try (ServerSocket socket = new ServerSocket(0)) {
-            new Thread(() -> listen(socket)).start();
+        runServer(0);
+    }
 
+    public void runServer(int port) {
+        try (ServerSocket socket = new ServerSocket(port)) {
+            System.out.println("Server now running on port: " + socket.getLocalPort());
+            listen(socket);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
-
 }
